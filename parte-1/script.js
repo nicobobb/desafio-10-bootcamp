8a161e054c9320c4c3ddb4372806ee06431a9adb
Nico Bobb <nicobobb@gmail.com>
2023-12-15 03:27:02 -0300

    add project

---
+++ parte-1/script.js
@@ -0,0 +1,38 @@
+class Libro {
+    constructor(titulo, autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    nombreCompleto() {
+        return `${this.titulo}, de ${this.autor.toUpperCase()}`;
+    }
+
+    mostrarDatosEnConsola() {
+        console.log(this.nombreCompleto());
+    }
+
+    mostrarDatosEnAlert() {
+        alert(this.nombreCompleto());
+    }
+
+    getTitulo() {
+        return this.titulo;
+    }
+
+    mostrarDatos(tipo) {
+        switch (tipo) {
+            case "console":
+                this.mostrarDatosEnConsola();
+                break;
+            case "alert":
+                this.mostrarDatosEnAlert();
+                break;
+            default:
+                this.mostrarDatosEnConsola();
+                break;
+        }
+    }
+}
+
+let unLibro = new Libro("Ángeles y Demonios", "Dan Brown");
